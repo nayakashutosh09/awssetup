3b75a21580390da5e09d117bc44cf6cfc6e7fb9f
nayakashutosh09 <147031468+nayakashutosh09@users.noreply.github.com>
2023-11-06 16:42:31 +0530

    Update installer1.sh

--- scripts/installer1.sh
+++ scripts/installer1.sh
@@ -2,7 +2,7 @@
 
 ACCOUNT2="559866444233"
 
-REPOSITORY_URI=559866444233.dkr.ecr.us-east-1.amazonaws.com/isr-table-generation
+REPOSITORY_URI=$ACCOUNT2.dkr.ecr.us-east-1.amazonaws.com/vsr-h2o-model
 
 echo "TASK:2"
 
@@ -14,5 +14,5 @@ echo "-----------------***************** AWS ECR LOGIN '$ACCOUNT2' : COMPLETED *
 
 echo "TASK:3"
 echo "-----------------***************** AWS ECR IMAGE PULLING : STARTED *****************-----------------"
-docker pull $ACCOUNT2.dkr.ecr.us-east-1.amazonaws.com/vsr-h2o-model:prod
+docker pull $REPOSITORY_URI:prod
 echo "-----------------***************** AWS ECR IMAGE PULLING : COMPLETED *****************-----------------"
