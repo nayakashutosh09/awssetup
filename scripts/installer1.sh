82479fc21755536b97b6e266cd25d7172d440050
nayakashutosh09 <147031468+nayakashutosh09@users.noreply.github.com>
2023-11-06 17:24:40 +0530

    Update installer1.sh

--- scripts/installer1.sh
+++ scripts/installer1.sh
@@ -11,15 +11,23 @@ REPOSITORY_URI=$ACCOUNT1.dkr.ecr.us-east-1.amazonaws.com/vsr-h2o-model
 echo "-----------------***************** AWS SSO CONFIGURATION FOR '$ACCOUNT1': STARTED *****************-----------------"
 yum install awscli
 aws --version
-#AWS_ACCESS_KEY_ID="enter"
-#AWS_SECRET_ACCESS_KEY="enter"
-#AWS_DEFAULT_REGION="us-east-1"
-#AWS_OUTPUT_FORMAT="json"
+AWS_ACCESS_KEY_ID="enter"
+AWS_SECRET_ACCESS_KEY="enter"
+AWS_DEFAULT_REGION="us-east-1"
+AWS_OUTPUT_FORMAT="json"
 
-#aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID
-#aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
-#aws configure set default.region $AWS_DEFAULT_REGION
-#aws configure set default.output $AWS_OUTPUT_FORMAT
+aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID
+aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
+aws configure set default.region $AWS_DEFAULT_REGION
+aws configure set default.output $AWS_OUTPUT_FORMAT
+
+echo "TASK:4"
+echo "-----------------***************** AWS ARTIFACTORY SETTING : STARTED *****************-----------------"
+PACKAGE_NAME="bayer-aws-sso"
+REPO_URL="https://$USERNAME:AKCp8pRQnKwMczfgg3WqNEAKTJMu7MDxCbb2hHtDeXxpSSmFBzXhyxKMARadKBAd5AG6Dddxa@artifactory.bayer.com/artifactory/aws2-pypi-prod-util/"
+TRUSTED_HOST="artifactory.bayer.com"
+pip3 install $PACKAGE_NAME --index-url $REPO_URL --trusted-host $TRUSTED_HOST
+echo "-----------------***************** AWS ARTIFACTORY SETTING : COMPLETED *****************-----------------"
 
 aws-sso --username $USERNAME --password $PASSWORD -a $ACCOUNT1
 echo "-----------------***************** AWS SSO CONFIGURATION FOR '$ACCOUNT1': COMPLETED *****************-----------------"
