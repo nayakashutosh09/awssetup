32a6c58d66d63168ace5fe063ef37cbf6b98738e
nayakashutosh09 <147031468+nayakashutosh09@users.noreply.github.com>
2023-11-06 16:09:19 +0530

    Update installer1.sh

--- scripts/installer1.sh
+++ scripts/installer1.sh
@@ -1,3 +1,21 @@
 #!/bin/bash
 
-echo "hello world"
+REGION="ZAF"
+REGION_COUNT="us-east-1"
+DIRECTORY_PATH="/Users/efzqd/Downloads/installer"
+USERNAME="ashutosh.nayak.ext@bayer.com"
+PASSWORD="ByrAshNa22!"
+ACCOUNT1="374898892977"
+ACCOUNT2="559866444233"
+
+echo "TASK:2"
+
+echo "-----------------***************** AWS ECR LOGIN '$ACCOUNT2' : STARTED *****************-----------------"
+password=$(aws ecr get-login-password --region us-east-1)
+echo "$password" | docker login --username AWS --password-stdin $ACCOUNT2.dkr.ecr.us-east-1.amazonaws.com
+echo "-----------------***************** AWS ECR LOGIN '$ACCOUNT2' : COMPLETED *****************-----------------"
+
+echo "TASK:3"
+echo "-----------------***************** AWS ECR IMAGE PULLING : STARTED *****************-----------------"
+docker pull $ACCOUNT2.dkr.ecr.us-east-1.amazonaws.com/vsr-h2o-model:prod
+echo "-----------------***************** AWS ECR IMAGE PULLING : COMPLETED *****************-----------------"
