df3a2e1aa934bfb7a705cdc4ba1e19195f60d526
nayakashutosh09 <147031468+nayakashutosh09@users.noreply.github.com>
2023-11-06 16:13:28 +0530

    Update installer1.sh

--- scripts/installer1.sh
+++ scripts/installer1.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 
-REGION="ZAF"
-REGION_COUNT="us-east-1"
-DIRECTORY_PATH="/Users/efzqd/Downloads/installer"
-USERNAME="ashutosh.nayak.ext@bayer.com"
-PASSWORD="ByrAshNa22!"
-ACCOUNT1="374898892977"
 ACCOUNT2="559866444233"
+
+REPOSITORY_URI=559866444233.dkr.ecr.us-east-1.amazonaws.com/isr-table-generation
 
 echo "TASK:2"
 
 echo "-----------------***************** AWS ECR LOGIN '$ACCOUNT2' : STARTED *****************-----------------"
-password=$(aws ecr get-login-password --region us-east-1)
-echo "$password" | docker login --username AWS --password-stdin $ACCOUNT2.dkr.ecr.us-east-1.amazonaws.com
+#password=$(aws ecr get-login-password --region us-east-1)
+#echo "$password" | docker login --username AWS --password-stdin $ACCOUNT2.dkr.ecr.us-east-1.amazonaws.com
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 374898892977.dkr.ecr.us-east-1.amazonaws.com
 echo "-----------------***************** AWS ECR LOGIN '$ACCOUNT2' : COMPLETED *****************-----------------"
 
 echo "TASK:3"
