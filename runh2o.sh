024e88444479341154dd57fc135f697264dbf06b
nayakashutosh09 <147031468+nayakashutosh09@users.noreply.github.com>
2023-11-07 23:10:21 +0530

    Create runh2o.sh

---
+++ runh2o.sh
@@ -0,0 +1,4 @@
+/vsr/pipeline/RunH2O.R --model /vsr/tmp/Mod_5.0_QC5 \
+                       --soildata /vsr/tmp/calibrateddata.csv \
+                       --auxdata /vsr/tmp/regionaldata.json \
+                       --ratedata /vsr/tmp/yieldpotential.csv \
